3f95cb50ea86aa0a9dcc7bd0fd3b286bc4686b89
Guest User <Guest@Epicodus-8A.local>
2018-05-29 15:07:47 -0700

    Add dark and light theme for users to toggle between.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+});
